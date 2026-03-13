4706fe2fd50dca1e168689009fcf853289e733e4
Techkeyy <israelolawale891@gmail.com>
2026-03-13 09:48:02 +0100

    Add walletConnect and coinbaseWallet connectors

--- chain.ts
+++ chain.ts
@@ -1,23 +1,24 @@
 import { defineChain } from 'viem'
 import { createConfig, http } from 'wagmi'
-import { injected } from 'wagmi/connectors'
+import { injected, walletConnect, coinbaseWallet } from 'wagmi/connectors'
 
 export const somniaTestnet = defineChain({
-  id: 50312,
-  name: 'Somnia Testnet',
-  network: 'somnia-testnet',
+  id: 50312, name: 'Somnia Testnet', network: 'somnia-testnet',
   nativeCurrency: { name: 'Somnia Test Token', symbol: 'STT', decimals: 18 },
   rpcUrls: {
     default: { http: ['https://dream-rpc.somnia.network'], webSocket: ['wss://dream-rpc.somnia.network/ws'] },
     public:  { http: ['https://dream-rpc.somnia.network'], webSocket: ['wss://dream-rpc.somnia.network/ws'] },
   },
-  blockExplorers: {
-    default: { name: 'Somnia Explorer', url: 'https://shannon-explorer.somnia.network' },
-  },
+  blockExplorers: { default: { name: 'Somnia Explorer', url: 'https://shannon-explorer.somnia.network' } },
 })
 
 export const wagmiConfig = createConfig({
   chains: [somniaTestnet],
-  connectors: [injected()],
+  connectors: [
+    injected(),
+    walletConnect({ projectId: 'b8a1daa2dd22335f4e2a5a2d3c9d9e1f' }),
+    coinbaseWallet({ appName: 'ReactPay' }),
+  ],
   transports: { [somniaTestnet.id]: http('https://dream-rpc.somnia.network') },
+  reconnectOnMount: false,
 })
