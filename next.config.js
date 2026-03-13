780b1cf82852b453eef8df80d9b8739cebfec9df
Techkeyy <israelolawale891@gmail.com>
2026-03-13 07:10:49 +0100

    Enhance next.config.js with webpack fallback settings
    Added webpack fallback configuration for specific modules.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false }
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+      'pino-pretty': false,
+    }
+    return config
+  },
+}
+
 module.exports = nextConfig
