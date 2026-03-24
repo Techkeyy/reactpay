11b385d9c4773ac23925932413a3991ba49bb8ee
Techkeyy <israelolawale891@gmail.com>
2026-03-24 07:05:44 +0100

    Add transfer function to MOCK_STT_ABI

--- contracts.ts
+++ contracts.ts
@@ -8,6 +8,7 @@ export const MOCK_STT_ABI = [
   { name: 'balanceOf',    type: 'function', stateMutability: 'view',        inputs: [{ name: 'account', type: 'address' }], outputs: [{ type: 'uint256' }] },
   { name: 'allowance',    type: 'function', stateMutability: 'view',        inputs: [{ name: 'owner', type: 'address' }, { name: 'spender', type: 'address' }], outputs: [{ type: 'uint256' }] },
   { name: 'approve',      type: 'function', stateMutability: 'nonpayable',  inputs: [{ name: 'spender', type: 'address' }, { name: 'amount', type: 'uint256' }], outputs: [{ type: 'bool' }] },
+  { name: 'transfer', type: 'function', stateMutability: 'nonpayable', inputs: [{ name: 'to', type: 'address' }, { name: 'amount', type: 'uint256' }], outputs: [{ type: 'bool' }] },
   { name: 'faucet',       type: 'function', stateMutability: 'nonpayable',  inputs: [{ name: 'amount', type: 'uint256' }], outputs: [] },
   { name: 'decimals',     type: 'function', stateMutability: 'view',        inputs: [], outputs: [{ type: 'uint8' }] },
   { name: 'symbol',       type: 'function', stateMutability: 'view',        inputs: [], outputs: [{ type: 'string' }] },
